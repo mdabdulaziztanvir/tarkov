9c7e64d78792bbb7bb0298ea666e787d8e71d00a
mdabdulaziztanvir <abdulazizztanvir@gmail.com>
2024-09-07 20:56:41 +0600

    Francais when user is on the english version and show English when the user is on the French version

--- home/lanChange.php
+++ home/lanChange.php
@@ -1,8 +1,8 @@
 <div class="lan__change-div">
 <label class="language__label" for="language-change"></label>
-
+<!-- now if he select fr he will see to change to en -->
 <select class= "language-change" id="language-change">
-  <option value="English">English</option>
-  <option value="French">Français</option>
+  <option value="English">Français</option>
+  <option value="French">English</option>
 </select>
 </div>
